f4eb59a702dd02f66e6ad6fcd7743e6d79eb32c6
kutlwano10 <kutlwano.ramotebele@gmail.com>
2024-09-26 16:06:50 +0200

    bug Fixed: i was passing the catch on the API wrong, i had to pass the no-store outside the backticks

--- app/api/route.js
+++ app/api/route.js
@@ -4,7 +4,7 @@ const API_BASE_URL = "https://next-ecommerce-api.vercel.app/products"
 export default async function fetchData (skip = 0, searchItem = "") {
     const searchQuery = searchItem ? `&search=${searchItem}` : "";
     
-    const response = await fetch(`${API_BASE_URL}?skip=${skip}${searchQuery}, ${{ cache: "no-store" }}`)
+    const response = await fetch(`${API_BASE_URL}?skip=${skip}${searchQuery}`, {catch: "no-store"})
 
     if(!response) {
         throw Error ("Failed to fetch Data")
